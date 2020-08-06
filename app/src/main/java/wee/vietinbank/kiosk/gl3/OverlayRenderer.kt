b4ad8b0f113274fd869e5c5d5472619a9e6fb191
Huy Quoc Vu <huyquocvu.sg@gmail.com>
2020-08-06 11:22:45 +0700

    commit

--- app/src/main/java/wee/vietinbank/kiosk/gl3/OverlayRenderer.kt
+++ app/src/main/java/wee/vietinbank/kiosk/gl3/OverlayRenderer.kt
@@ -18,7 +18,7 @@ class OverlayRenderer : GLSurfaceView.Renderer {
 
     private val rotationMatrix = FloatArray(16)
 
-    private var drawObj : Cube? = null
+    private var drawObj : Obj? = null
 
     private var width = 0
 
@@ -96,7 +96,7 @@ class OverlayRenderer : GLSurfaceView.Renderer {
         //set the clear buffer color to light gray.
         GLES31.glClearColor(0f, 0f, 0f, 1f)
         //initialize the cube code for drawing.
-        drawObj = Cube()
+        drawObj = Obj()
         //if we had other objects setup them up here as well.
     }
 
